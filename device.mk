6312ca6a5ac10f32f0333f2d6f2598563a41dd19
Joonhyuk Song <fprhqkrtk303@naver.com>
2015-12-12 16:46:25 +0900

    f400: add missing overlay config
    Change-Id: Ibf8edf30c639f6f236a9806cf8a2bdc2367b17ec

--- device.mk
+++ device.mk
@@ -27,6 +27,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
