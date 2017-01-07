ab5df1dad0f3470ed823ae3ca2dabb2f70f14ca1
Hendrik Hagendorn <git@finnq.de>
2017-01-07 04:11:49 +0300

    f400: lunch target: cm->lineage
    Change-Id: I0ce74528c277f9ee7f10d995343fc4aac2fcdedd

--- lineage.mk
+++ lineage.mk
@@ -3,6 +3,10 @@ $(call inherit-product, device/lge/f400/full_f400.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_f400
+PRODUCT_NAME := lineage_f400
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_kt_kr" BUILD_FINGERPRINT="lge/g3_kt_kr/g3:5.0/LRX21R/F400K20f.1419232865:user/release-keys" PRIVATE_BUILD_DESC="g3_kt_kr-user 5.0 LRX21R F400K20f.1419232865 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_kt_kr" \
+    BUILD_FINGERPRINT="lge/g3_kt_kr/g3:5.0/LRX21R/F400K20f.1419232865:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_kt_kr-user 5.0 LRX21R F400K20f.1419232865 release-keys"
